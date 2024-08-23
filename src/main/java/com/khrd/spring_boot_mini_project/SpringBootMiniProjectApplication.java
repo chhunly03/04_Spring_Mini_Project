7d284171b9ec0ccf137848f7ed3b5a78662075b6
Chorn Chhunly <chhunlychorn97@gmail.com>
2024-08-23 15:41:35 +0700

    create structure

---
+++ src/main/java/com/khrd/spring_boot_mini_project/SpringBootMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.khrd.spring_boot_mini_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMiniProjectApplication.class, args);
+    }
+
+}
