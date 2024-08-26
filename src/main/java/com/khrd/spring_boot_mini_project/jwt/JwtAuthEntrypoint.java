fa9c8e0d4c6e3c5401999c6f37aeaff2495b206b
PhannSothyrith <phannsothyrith@gmail.com>
2024-08-26 10:39:03 +0700

    change something

--- src/main/java/com/khrd/spring_boot_mini_project/jwt/JwtAuthEntrypoint.java
+++ src/main/java/com/khrd/spring_boot_mini_project/jwt/JwtAuthEntrypoint.java
@@ -1,10 +1,18 @@
 package com.khrd.spring_boot_mini_project.jwt;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import javax.naming.AuthenticationException;
 import java.io.IOException;
-
 @Component
-public class JwtAuthEntrypoint{
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
 
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
