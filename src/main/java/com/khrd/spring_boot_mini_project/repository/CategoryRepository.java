afa07bcec2f04b0a3c610ff620a21f99a9767274
PhannSothyrith <phannsothyrith@gmail.com>
2024-08-27 17:08:45 +0700

    change create article

--- src/main/java/com/khrd/spring_boot_mini_project/repository/CategoryRepository.java
+++ src/main/java/com/khrd/spring_boot_mini_project/repository/CategoryRepository.java
@@ -2,6 +2,11 @@ package com.khrd.spring_boot_mini_project.repository;
 
 import com.khrd.spring_boot_mini_project.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+   List<Category> findCategoriesByUserUserId(Integer userId);
 }
